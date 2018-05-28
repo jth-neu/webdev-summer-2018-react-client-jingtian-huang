22428ca163ae9591ac9e8f8d47fc43bb812a062c
Jingtian Huang <huang.jin@husky.neu.edu>
2018-05-28 15:10:18 -0700

    Add Lesson tabs

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CourseCard from '../components/CourseCard';
 import ModuleList from './ModuleList';
+import LessonTabs from './LessonTabs';
 
 export default class CourseManager
     extends React.Component {
@@ -8,6 +9,7 @@ export default class CourseManager
         return (
             <div className="container-fluid">
                 <h1>Course Manager</h1>
+                <LessonTabs/>
                 <ModuleList/>
                 <CourseCard/>
             </div>
