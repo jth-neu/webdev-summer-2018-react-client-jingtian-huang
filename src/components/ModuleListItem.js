e96199f5a263ccafb8d2c1e71d96cabe6d3849f1
Jingtian Huang <huang.jin@husky.neu.edu>
2018-05-28 14:57:51 -0700

    Add Module list item and modulelist

---
+++ src/components/ModuleListItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class ModuleListItem
+    extends React.Component {
+    render() {
+        return (
+            <li className="list-group-item">
+                Module 1
+            </li>
+        );
+    }
+}
--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import CourseCard from './CourseCard'
+import React from 'react';
+import CourseCard from '../components/CourseCard';
+import ModuleList from './ModuleList';
 
 export default class CourseManager
     extends React.Component {
@@ -7,6 +8,7 @@ export default class CourseManager
         return (
             <div className="container-fluid">
                 <h1>Course Manager</h1>
+                <ModuleList/>
                 <CourseCard/>
             </div>
         )
