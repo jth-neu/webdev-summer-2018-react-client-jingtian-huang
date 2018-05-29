13e36e1faa8ae0b3638179036f91264e41f39da9
Jingtian Huang <huang.jin@husky.neu.edu>
2018-05-29 16:52:49 -0700

    Add form in module list

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import HelloWorld from './hello';
 import CourseManager from './containers/CourseManager'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import '../node_modules/font-awesome/css/font-awesome.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css'
+import ModuleList from "./containers/ModuleList"
 
 ReactDOM.render(
-    <CourseManager/>,
+    <div className="container-fluid">
+        <ModuleList/>
+        <CourseManager/>
+    </div>,
     document.getElementById('root')
 );
