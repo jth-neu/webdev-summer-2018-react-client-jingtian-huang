3b03e32c6cecaadd774fe06176b1da9a75df7e51
Jingtian Huang <huang.jin@husky.neu.edu>
2018-06-01 14:51:43 -0700

    Change module font

--- src/components/ModuleListItem.js
+++ src/components/ModuleListItem.js
@@ -9,7 +9,7 @@ export default class ModuleListItem
         return (
             <li className="list-group-item"
                 onClick={(event,moduleId = this.props.module.id)=>this.props.setModule(event,moduleId)}>
-                {this.props.title}
+                <label className="col-form-label font-weight-bold">{this.props.title}</label>
                 <span className="float-right">
                     <button type="button" className="btn btn-danger"
                             onClick={(event,moduleId = this.props.module.id)=>this.props.remove(event,moduleId)}>
