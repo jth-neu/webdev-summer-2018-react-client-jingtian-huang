1eb6de463c7f317ff6c235d6165001e42f36c2bd
Jingtian Huang <huang.jin@husky.neu.edu>
2018-06-14 18:12:47 -0700

    Import redux in index and remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import HelloWorld from './hello';
 import CourseManager from './containers/CourseManager'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/font-awesome/css/font-awesome.min.css'
-import ModuleList from "./containers/ModuleList"
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 
 ReactDOM.render(
-    <div className="container-fluid">
-        {/*<ModuleList/>*/}
-        <CourseManager/>
-    </div>,
+    <Provider>
+        <div className="container-fluid">
+            <CourseManager/>
+        </div>
+    </Provider>,
     document.getElementById('root')
 );
