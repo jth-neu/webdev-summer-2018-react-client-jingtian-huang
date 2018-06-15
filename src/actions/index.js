138c982d4a3526be72355c9e5c6a60734e3c2ff7
Jingtian Huang <huang.jin@husky.neu.edu>
2018-06-15 15:59:14 -0700

    Add findAllWidgets action

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+import * as constants from "../constants/index"
+
+export const findAllWidgets = dispatch => {
+    fetch('http://localhost:8080/api/widget')
+        .then(response => (response.json()))
+        .then(widgets => dispatch({
+            type: constants.FIND_ALL_WIDGETS,
+            widgets: widgets }))
+}
