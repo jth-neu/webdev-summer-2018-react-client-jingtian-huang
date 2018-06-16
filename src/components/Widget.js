f2b07a12dad750918c3325629f47a2341bca5ba6
Jingtian Huang <huang.jin@husky.neu.edu>
2018-06-15 17:09:27 -0700

    Add different widget components

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -21,9 +21,44 @@ const Widget = ({widget, dispatch}) => {
             <button onClick={e => (
                 dispatch({type: 'DELETE_WIDGET', id: widget.id})
             )} >Delete</button>
+            <div>
+                {widget.widgetType==='Heading' &&  <Heading/>}
+                {widget.widgetType==='Paragraph' && <Paragraph/>}
+                {widget.widgetType==='List' && <List/>}
+                {widget.widgetType==='Link' && <Link/>}
+                {widget.widgetType==='Image' && <Image/>}
+            </div>
         </li>
     )
 };
+
+const Heading = () => (
+    <div>
+    <h2>Heading</h2>
+        <select>
+            <option>Heading 1</option>
+            <option>Heading 2</option>
+            <option>Heading 3</option>
+        </select>
+    </div>
+)
+
+const Paragraph = () => (
+    <div>
+        <h2>Paragraph</h2>
+        <textarea></textarea>
+    </div>
+)
+const List = () => (
+    <h2>List</h2>
+)
+const Image = () => (
+    <h2>Image</h2>
+)
+const Link = () => (
+    <h2>Link</h2>
+)
+
 
 const WidgetContainer = connect()(Widget);
 
